f6b19fb0535aa90cf37b8d854b10f85587690a72
Mariana Brito <mariana.bsouza@hotmail.com>
2019-01-26 16:17:28 -0200

    created link component

---
+++ src/components/Link/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class Link extends Component {
+  render() {
+    return (
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React today
+        </a>
+    );
+  }
+}
+
+export default Link;
