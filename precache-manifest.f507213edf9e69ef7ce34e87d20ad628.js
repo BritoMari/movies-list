4b3be3de6fac1f253dad891dd6a30bae3abdc491
Mariana Brito <mariana.bsouza@hotmail.com>
2019-01-27 13:39:09 -0200

    Updates

---
+++ precache-manifest.f507213edf9e69ef7ce34e87d20ad628.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ddf35eda7583c06331c8",
+    "url": "/movies-list/static/js/runtime~main.ddf35eda.js"
+  },
+  {
+    "revision": "90c7f41a58bd0b75b4c4",
+    "url": "/movies-list/static/js/main.90c7f41a.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/movies-list/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "90c7f41a58bd0b75b4c4",
+    "url": "/movies-list/static/css/main.61acd415.chunk.css"
+  },
+  {
+    "revision": "25fc9cf395701735e9e5f9c0d598414f",
+    "url": "/movies-list/index.html"
+  }
+];
