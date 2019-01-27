38afba3ddbcde86ca4d16e81e1a81a201f6a03cc
Mariana Brito <mariana.bsouza@hotmail.com>
2019-01-27 12:48:13 -0200

    moving the fetch function to parent component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,39 @@ import MoviesList from './components/MoviesList';
 import Search from './components/Search';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://www.omdbapi.com/?apikey=7c323e6e&s=harry-potter&type=movie")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.Search
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
   render() {
+    const { error, isLoaded, items } = this.state;
     return (
       <div className="App">
         <header className="App-header">
@@ -14,7 +46,7 @@ class App extends Component {
           </h1>
         </header>
         <Search />
-        <MoviesList />
+        <MoviesList error={error} isLoaded={isLoaded} items={items} />
         <footer className="App-footer">
           <p className="App-created">Created by Mariana Brito, <Link /></p>
         </footer>
--- src/components/MoviesList/index.js
+++ src/components/MoviesList/index.js
@@ -2,39 +2,10 @@ import React, { Component } from 'react';
 import './style.css';
 
 class MoviesList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      items: []
-    };
-  }
-
-  componentDidMount() {
-    fetch("http://www.omdbapi.com/?apikey=7c323e6e&s=harry-potter&type=movie")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            items: result.Search
-          });
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-  }
 
   render() {
-    const { error, isLoaded, items } = this.state;
+    const { error, isLoaded, items } = this.props;
+
     if (error) {
       return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
