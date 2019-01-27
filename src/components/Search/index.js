0e1cc3defdc24f6b3c4eed898bc4125f1e52b26c
Mariana Brito <mariana.bsouza@hotmail.com>
2019-01-27 12:33:57 -0200

    capturing onChange and onClick events in the search

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react';
 import './style.css';
 
 class Search extends Component {
+  state = {
+    search: '',
+  }
+
+  valueChange = event  => {
+    console.log(event.target.value);
+
+    this.setState({
+      search: event.target.value
+    });
+  }
+
+  sendSearch = event => {
+    console.log('buscar na api: ', this.state.search);
+  }
+
   render() {
     return (
       <div className="Search">
-        <input type="text" className="Search-input" placeholder="Search..."/>
-        <button className="Search-buttton">go</button>
+        <input value={this.state.search} onChange={this.valueChange} type="text" className="Search-input" placeholder="Search..."/>
+        <button onClick={this.sendSearch} className="Search-buttton">go</button>
         <p className="Search-message">Use the field above to search for movies, for example: Harry Potter</p>
       </div>
     );
